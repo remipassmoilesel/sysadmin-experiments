ae25557a41fa3ad5f19cb7d2ed12e1692d218211
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-11-24 13:24:37 +0100

    Improve helper

--- keycloak/tests/helper.js
+++ keycloak/tests/helper.js
@@ -1,7 +1,8 @@
 const request = require('request-promise');
 
 const config = {
-    url: '/realms/master/protocol/openid-connect/token',
+    openidConnectPath: '/realms/master/protocol/openid-connect/token',
+    evaluatePath: '/admin/realms/library-poc/clients/e47e0f0d-2932-4d7f-8533-1f7eac9305cf/authz/resource-server/policy/evaluate',
     baseUrl: 'http://172.17.0.3:8080/auth',
     username: 'admin',
     password: 'admin',
@@ -17,7 +18,7 @@ const helper = {
 
         const options = {
             method: 'POST',
-            uri: settings.baseUrl + settings.url,
+            uri: settings.baseUrl + settings.openidConnectPath,
             form: settings,
             json: true
         };
@@ -47,14 +48,47 @@ const helper = {
         });
     },
 
-    evaluate: (settings) => {
+    evaluate: (settings, payload) => {
+
+        return helper.getAuth(settings).then((auth) => {
+
+            const options = {
+                method: 'POST',
+                uri: settings.baseUrl + settings.evaluatePath,
+                auth: auth,
+                body: payload,
+                json: true
+            };
+
+            console.log(options);
+
+            return request(options);
+        });
 
     },
 
 };
 
-helper.getRealms(config).then((auth) => {
-    console.log(auth)
-});
+// helper.getRealms(config).then((auth) => {
+//     console.log(auth)
+// });
+
+const payload = {
+    "resources": [{
+        "name": "library_a",
+        "uri": "/library-a",
+        "type": "library-api:library",
+        "owner": {"id": "e47e0f0d-2932-4d7f-8533-1f7eac9305cf", "name": "library_api_client"},
+        "_id": "6ae3b27b-4b43-413b-b5f9-81d79a89a189",
+        "scopes": ["Edit"]
+    }],
+    "context": {"attributes": {}},
+    "roleIds": ["library_admin"],
+    "clientId": "e47e0f0d-2932-4d7f-8533-1f7eac9305cf",
+    "userId": "2c594d05-6ecb-4f86-9df8-ca9933aec6ba",
+    "entitlements": false
+};
+
+helper.evaluate(config, payload);
 
 module.exports = helper;
