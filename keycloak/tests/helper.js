3da694b5729982acdf7534e500583dc98460c520
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2017-11-24 13:18:24 +0100

    Improve helper

--- keycloak/tests/helper.js
+++ keycloak/tests/helper.js
@@ -1,23 +1,60 @@
-const kc = require('keycloak-admin-client');
+const request = require('request-promise');
 
-module.exports = {
+const config = {
+    url: '/realms/master/protocol/openid-connect/token',
+    baseUrl: 'http://172.17.0.3:8080/auth',
+    username: 'admin',
+    password: 'admin',
+    grant_type: 'password',
+    client_id: 'admin-cli'
+};
 
-    connect: (settings) => {
-        kc(settings)
-            .then((client) => {
-                client.realms.find()
-                    .then((realms) => {
-                        console.log('realms', realms);
-                    });
-            })
-            .catch((err) => {
-                console.log('Error', err);
-            });
+const helper = {
+
+    settings: {},
+
+    getToken: (settings) => {
+
+        const options = {
+            method: 'POST',
+            uri: settings.baseUrl + settings.url,
+            form: settings,
+            json: true
+        };
+
+        return request(options).then((data) => {
+            return data.access_token;
+        });
+    },
+
+    getAuth: (settings) => {
+        return helper.getToken(settings).then((accessToken) => {
+            // console.log(arguments);
+            return {
+                bearer: accessToken
+            };
+        });
+    },
+
+    getRealms: (settings) => {
+        return helper.getAuth(settings).then((auth) => {
+            const options = {
+                uri: `${settings.baseUrl}/admin/realms`,
+                auth: auth,
+                json: true
+            };
+            return request(options);
+        });
     },
 
     evaluate: (settings) => {
 
     },
 
-
 };
+
+helper.getRealms(config).then((auth) => {
+    console.log(auth)
+});
+
+module.exports = helper;
