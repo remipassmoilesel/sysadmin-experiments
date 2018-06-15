ecbd898c3df72b6f685303c434283bf85ac3da7d
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-06-15 09:28:29 +0200

    Add example mount point, move note to linux utils

---
+++ glusterfs/mount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# mercredi 13 juin 2018, 13:47:30 (UTC+0200)
+
+# You will need glusterfs-client package !
+
+set -x
+
+mkdir example-mount-point
+
+sudo mount -t glusterfs 10.0.1.30:/repl-vol $(pwd)/example-mount-point
