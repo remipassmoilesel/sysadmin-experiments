a8aa73cbcab291d1783a5c5abdbf5f998025b2d5
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-08-11 15:00:04 +0200

    Add create merge request call

--- gitlab-ce/private-access-token/apiV4.js
+++ gitlab-ce/private-access-token/apiV4.js
@@ -5,7 +5,7 @@ const config = require('./config');
 console.log('Using config: ' + JSON.stringify(config, null, 2));
 
 const printResponse = (resp) => {
-  console.log(JSON.stringify(JSON.parse(resp.body), null, 2));
+  console.log(JSON.stringify(resp.body, null, 2));
 }
 
 const getUser = async () => {
@@ -15,7 +15,8 @@ const getUser = async () => {
   const user = await got(url, {
     headers: {
       'Private-Token': config.accessToken
-    }
+    },
+    json: true,
   });
 
   printResponse(user);
@@ -28,7 +29,8 @@ const listUserProjects = async () => {
   const projects = await got(url, {
     headers: {
       'Private-Token': config.accessToken
-    }
+    },
+    json: true,
   });
 
   printResponse(projects);
@@ -41,7 +43,37 @@ const listMergeRequests = async () => {
   const mergeRequests = await got(url, {
     headers: {
       'Private-Token': config.accessToken
-    }
+    },
+    json: true,
+  });
+
+  printResponse(mergeRequests);
+}
+
+const createMergeRequest = async () => {
+  console.log('Creating merge request: ');
+
+  // POST /projects/:id/merge_requests
+  const sourceBranch = 'master';
+  const targetBranch = 'env/preprod';
+  const title = "Update of " + targetBranch
+  const description = "MR description"
+
+  const url = config.baseUrl + '/projects/' + config.projectId + '/merge_requests'
+  const mergeRequests = await got(url, {
+    method: 'POST',
+    headers: {
+      'Private-Token': config.accessToken
+    },
+    body: {
+      id: config.projectId,
+      source_branch: sourceBranch,
+      target_branch: targetBranch,
+      title,
+      description,
+      squash: true,
+    },
+    json: true,
   });
 
   printResponse(mergeRequests);
@@ -54,6 +86,8 @@ const main = async () => {
     await getUser();
   } else if (process.argv[2] === 'list-mr') {
     await listMergeRequests();
+  } else if (process.argv[2] === 'create-mr') {
+    await createMergeRequest();
   } else {
     throw new Error('You must provide a command');
   }
