4d37de3ccd9f6b0a9189d442ff24c518b8203acf
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-08-12 11:25:18 +0200

    Rename dir

---
+++ gitlab-ce/personnal-access-token/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    username: 'remipassmoilesel',
+    userId: '2662891',
+    projectId: '7766703',
+    accessToken: 'YBk1_biR2kyHALgQpoxb',
+    baseUrl: 'https://gitlab.com/api/v4'
+}
